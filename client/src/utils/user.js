66c4aac7c4b93beec10c4d5766137dd0d581cb17
quangmindx <quangtmps12135@fpt.edu.vnquangtmps12135@fpt.edu.vnquangtmps12135@fpt.edu.vn>
2021-06-06 00:40:39 +0700

    fix conversations error

--- client/src/utils/user.js
+++ client/src/utils/user.js
@@ -1,29 +1,30 @@
 import { REQUEST_URL } from "../constants/global";
-export const getUserById = async (id) => {
-  return fetch(`${REQUEST_URL}/user/:${id}`)
+
+export function getUserById(id) {
+  return fetch(`${REQUEST_URL}/user/${id}`)
     .then((res) => res.json())
     .then((data) => {
       const { avatar, fullName } = data;
       return { avatar, fullName };
     });
-};
+}
 
-export const getConversationByUserId = async (id) => {
-  const res = await fetch(`${REQUEST_URL}/conversation/${id}`);
-  const conversation = await res.json();
+export async function getConversationByUserId(id) {
+  try {
+    const res = await fetch(`${REQUEST_URL}/conversation/${id}`);
+    const conversations = await res.json();
 
-  const userPromise = conversation.data.map(async (con) => {
-    let receiverId = con.usersId.join("").replace(id, "");
-    const receiverInfo = await getUserById(receiverId);
-    return {
-      conversationId: con._id,
-      ...receiverInfo,
-    };
-  });
+    const usersPromise = conversations.data.map(async (con) => {
+      let receiverId = con.usersId.join("").replace(id, "");
+      const receiverInfo = await getUserById(receiverId);
+      return {
+        conversationId: con._id,
+        ...receiverInfo,
+      };
+    });
 
-  return Promise.all(userPromise).then((res) => {
-    console.log(res);
-  });
-
-  // return await Promise.all(userPromise);
-};
+    return await Promise.all(usersPromise);
+  } catch (error) {
+    console.log(error);
+  }
+}
